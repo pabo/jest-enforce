037daba44c56b54fb9ec8105ff0d5b3ac603d6e1
Brett Schellenberg <brett.schellenberg@gmail.com>
2017-10-29 17:25:22 -0700

    add configuration

--- README.md
+++ README.md
@@ -14,8 +14,22 @@ $ jest-enforce
 ### Functionality
 The command line util calls `jestEnforce.checkMockImports()`, which inspects all `.spec` files under `./src` and tells you if you are not mocking modules you should be mocking.
 
+### Configuration
+You can add a configuration to your `package.json` file:
+```
+"jest-enforce": {
+  "whitelistedLibraries": [
+    "react",
+    "react-redux",
+    "styled-components",
+    "lodash",
+    "import {", //TODO this is because I don't yet handle multi line imports
+  ] 
+}
+```
+
 ### TODO
-- allow configuration
-  - what imports are OK to ignore
-- fix multiline import statements
-- colorize output
+- [x] allow configuration
+  - [x] what imports are OK to ignore
+- [ ] fix multiline import statements
+- [ ] colorize output
--- build/bundle.js
+++ build/bundle.js
@@ -2,50 +2,51 @@
 
 // const recursive = require('recursive-readdir');
 // const readline = require('readline');
-var findInFiles = require('find-in-files');
+const fs = require('fs');
+const findInFiles = require('find-in-files');
 
-// it's ok not to mock these
-const whitelistedLibs = [
-  'react',
-  'react-redux',
-  'styled-components',
-  'lodash',
-  'import {', //TODO this is because I don't yet handle multi line imports
-];
+// read config from package.json
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const config = packageJson['jest-enforce'];
+console.log(config);
+const { whitelistedLibraries = [] } = config;
 
 function checkMockImports() {
+  Promise.all([
+    findInFiles.find('jest.mock', './src', /\.spec\.js$/),
+    // TODO negative lookbehind not working
+    // TODO I'm getting all the .js files - not just the ones for which there are unit tests. Could optimize
+    findInFiles.find('import ', './src', /(?:(?!spec\.js$).)*\.js$/)
+  ]).then(([unitTestFiles, codeFiles]) => {
+    for (const mockFileName in unitTestFiles) {
+      const realLibFileName = mockFileName.replace(/\.spec/, '');
 
-Promise.all([
-  findInFiles.find('jest.mock', './src', /\.spec\.js$/),
-  // TODO negative lookbehind not working
-  // TODO I'm getting all the .js files - not just the ones for which there are unit tests. Could optimize
-  findInFiles.find('import ', './src', /(?:(?!spec\.js$).)*\.js$/) 
-]).then(([unitTestFiles, codeFiles]) => {
-  for (const mockFileName in unitTestFiles) {
-    const realLibFileName = mockFileName.replace(/\.spec/, '');
+      const mockedLibs = new Set(unitTestFiles[mockFileName].line.map((line) => {
+        return line.replace(/.*jest.mock\('(.*)'.*/, '$1');
+      }));
 
-    const mockedLibs = new Set(unitTestFiles[mockFileName].line.map((line) => {
-      return line.replace(/.*jest.mock\('(.*)'.*/, '$1');
-    }));
-
-    const importedLibs = new Set(codeFiles[realLibFileName].line.map((line) => {
-      // import analytics from '../../utils/analytics';
-      return line.replace(/.*import.*from ['"](.*)['"].*/, '$1');
-    }));
+      const importedLibs = new Set(codeFiles[realLibFileName].line.map((line) => {
+        // import analytics from '../../utils/analytics';
+        return line.replace(/.*import.*from ['"](.*)['"].*/, '$1');
+      }));
 
 
-    const difference = _differenceWithWhiteList(importedLibs, mockedLibs, whitelistedLibs);
-    if (difference.size) {
-      console.log(`${realLibFileName}'s spec is missing these mocks:`, difference );
+      const difference = _differenceWithWhiteList(importedLibs, mockedLibs, whitelistedLibraries);
+      if (difference.size) {
+        console.log(`${realLibFileName}'s spec is missing these mocks: ${_prettifySet(difference)}`);
+      }
     }
-  }
-}).catch((e) => {
-  console.log(`error: ${e}`);
-});
+  }).catch((e) => {
+    console.trace(`error: ${e}`);
+  });
+}
+
+function _prettifySet(set) {
+  return [...set].join(", ");
 }
 
 function _differenceWithWhiteList(set1, set2, whitelist) {
-  const whiteSet = new Set([...whitelist]);
+  const whiteSet = new Set(whitelist);
 
   const difference = new Set([...set1].filter(x => !set2.has(x)));
 
